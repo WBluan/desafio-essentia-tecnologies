cdbf1505acd1d01a82597cf6132a084dc3ad8ae2
WBluan <luan@lwbdev.localdomain>
2025-11-08 16:48:21 -0300

    fix: form component must recive formGroup property

--- frontend/src/app/shared/components/form/form.ts
+++ frontend/src/app/shared/components/form/form.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgStyle } from '@angular/common';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [NgStyle],
+  standalone: true,
+  imports: [NgStyle, ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -11,4 +13,6 @@ import { NgStyle } from '@angular/common';
 export class Form {
   @Input() gap = '1rem';
   @Input() align = 'stretch';
+  @Input() formGroup!: FormGroup;
+  @Output() submitted = new EventEmitter<void>();
 }

94bbd2d2f39a44a8dea13935459e30ff5ba6a233
WBluan <luan@lwbdev.localdomain>
2025-11-08 16:53:06 -0300

    feat: add todo export module and route for todo screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginForm } from './features/auth/login/login-form';
 import { Register } from './features/auth/register/register';
+import { TodoComponent } from './features/todo/todo-component/todo-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginForm },
-  { path: 'register', component: Register },];
+  { path: 'register', component: Register },
+  { path: 'todo', component: TodoComponent }
+];

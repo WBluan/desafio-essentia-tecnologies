cdfad50f7006cce4aae8ac161292d91d25b489c1
WBluan <luan@lwbdev.localdomain>
2025-11-07 23:50:49 -0300

    feat: add base component page and text-link

---
+++ frontend/src/app/shared/components/page/page.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-page',
+  imports: [NgStyle],
+  templateUrl: './page.html',
+  styleUrl: './page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Page {
+  @Input() contentMaxWidth = '480px';
+  @Input() centered = true;
+}

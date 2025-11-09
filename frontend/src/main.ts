14c802269e67bcf0ba454e945157719749f9515b
WBluan <luan@lwbdev.localdomain>
2025-11-09 17:10:53 -0300

    frontend-feat: add auth-interceptor for redirect unauthorized users

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi,  } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { appConfig } from './app/app.config';
+import { AuthInterceptor } from './app/core/interceptors/auth.interceprot';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
-    provideHttpClient(), 
+    provideHttpClient((withInterceptorsFromDi())),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     ...appConfig.providers
   ]
 }).catch(err => console.error(err));

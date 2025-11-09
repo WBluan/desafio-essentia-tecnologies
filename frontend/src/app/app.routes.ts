2ee269532722ce111a772bf901e730aa9b5580b3
WBluan <luan@lwbdev.localdomain>
2025-11-09 01:56:59 -0300

    frontend-feat: add auth guard to protect routes when user is not logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginForm } from './features/auth/login/login-form';
-import { Register } from './features/auth/register/register';
-import { TodoComponent } from './features/todo/todo-component/todo-component';
+import { Register } from './features/auth/components/register/register';
+import { TodoComponent } from './features/todo/components/todo-component/todo-component';
+import { LoginForm } from './features/auth/components/login/login-form';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginForm },
   { path: 'register', component: Register },
-  { path: 'todo', component: TodoComponent }
+  { path: 'todo', component: TodoComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '/login' }
 ];

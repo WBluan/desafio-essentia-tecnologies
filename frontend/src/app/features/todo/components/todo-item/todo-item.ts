1425250ae6e186f202a46b27157109ea3688063c
WBluan <luan@lwbdev.localdomain>
2025-11-09 17:29:22 -0300

    frontend-feat: add custom checkbox and input completed style

--- frontend/src/app/features/todo/components/todo-item/todo-item.ts
+++ frontend/src/app/features/todo/components/todo-item/todo-item.ts
@@ -5,18 +5,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormInput } from "../../../../shared/components/input/form-input";
+import { CustomCheckbox } from "../../../../shared/components/custom-checkbox/custom-checkbox";
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [Button, MatIconModule, FormsModule, CommonModule, FormInput],
+  imports: [Button, MatIconModule, FormsModule, CommonModule, FormInput, CustomCheckbox],
   templateUrl: './todo-item.html',
   styleUrls: ['./todo-item.scss'],
 })
 export class TodoItem {
   @Input() todo!: Todo;
   @Output() delete = new EventEmitter<number>();
-  @Output() toggleComplete = new EventEmitter<boolean>();
+  @Output() toggleComplete = new EventEmitter<{ completed: boolean }>();
   @Output() saveEdit = new EventEmitter<string>();
 
   editing = false;
